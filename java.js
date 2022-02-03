b7909141429cb71da24061c02dd953420133fd85
fc-pescara <88330165+fc-pescara@users.noreply.github.com>
2022-02-03 15:27:45 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,82 @@
+ /////////////// SELECT ////////////
+ 
+ let euro = document.getElementById('euro');
+ let euro5 = document.getElementById('euro5');
+ let dollar = document.getElementById('dollar');
+ let dollar5 = document.getElementById('dollar5');
+ let dollarCanadien = document.getElementById('dollarCanadien');
+let dollarcanadien5 = document.getElementById('dollarCanadien5');
+let livresterling = document.getElementById('livresterling');
+let livresterling5 = document.getElementById('livresterling5');
+let francuisse = document.getElementById('francuisse');
+let francuisse5 = document.getElementById('francsuisse5');
+
+///////////////////// SELECT ///////////
+let euroDollar = 1.13          // dollar //
+ let euroCanadien = 1.43 // dollar canadien //
+ let  euroLivre = 0.83 // livre sterling
+ let euroFranc =  1.04 // franc suisse //
+
+
+ francsuisseEuro = 0.96    // euro //
+francsuisseDollar =  1.08  // Dollar //
+francsuisseCanadien =  1.37  // Canadien
+francsuisseLivre  =0.80  // Livre
+
+
+livresterlingFranc = 1.25  // Franc Suisse //
+livresterlingEuro  = 1.20  // Euro //
+livresterlingDollar  = 1.35 // Dollar //
+livresterlingCanadien = 1.71 // Canadien //
+
+
+
+dollaramericainEuro = 0.89 // euro //
+dollaramericainCanadien = 1.27 // Canadien //
+dollaramericainSterling = 0.74  // Sterling //
+dollaramericainFranc = 0.92 // Franc Suisse
+
+
+dollarcanadienSuisse = 0.73 // Suisse //
+dollarcanadienEuro = 0.70 // euro //
+dollardcanadienAmericain = 0.79 // Americain //
+dollardcanadienSterling = 0.58 // Sterling //
+
+///////////////// ECHANGE //////////
+class transaction {
+    constructor(valeur1,valeur2,taux) {
+        this.valeur1 = valeur1 ;
+        this.valeur2 = valeur2;
+        this.taux = taux ;
+   if(valeur1 < 2 ) {
+       addEventListener.valeur1();{
+           console.log("bonjour tout le monde")
+    }
+    
+    
+       
+        
+    
+   }
+}
+
+    }
+
+
+let prototype =  new transaction (euro,dollar5,euroDollar);
+/////:ALGORYTMNE  DEJA CONSTRUIT //////////
+nouvelleaction(euro,dollarCanadien5,euroCanadien);
+nouvelleaction(euro,livresterling5,euroLivre);
+nouvelleaction(euro,francuisse5,euroFranc);
+nouvelleaction(dollar,dollarcanadien5,dollaramericainCanadien);
+nouvelleaction(dollar,euro5,dollaramericainEuro);
+nouvelleaction(dollar,livresterling5,dollaramericainSterling);
+nouvelleaction(dollar,francsuisse5,dollaramericainFranc);
+nouvelleaction(francuisse,euro5,francsuisseEuro);
+nouveleaction(francuisse,dollarCanadien5,francsuisseCanadien);
+nouvelleaction(francuisse,dollar5,francsuisseDollar);
+nouvelleaction(francuisse,livresterling5,francsuisseLivre);
+nouvelleaction(livresterling,dollarCanadien,livresterlingCanadien,);
+nouvelleaction(livresterling,euro5,livresterlingDollar);
+nouvelleaction(livresterling,dollar5,livresterlingEuro);
+nouvelleaction(livresterling,francsuisse5,livresterlingFranc);
