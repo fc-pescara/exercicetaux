a7abe5195d01b368b416887e7b60bc0f44611556
fc-pescara <88330165+fc-pescara@users.noreply.github.com>
2022-02-06 19:37:34 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -1,82 +1,31 @@
- /////////////// SELECT ////////////
- 
- let euro = document.getElementById('euro');
- let euro5 = document.getElementById('euro5');
- let dollar = document.getElementById('dollar');
- let dollar5 = document.getElementById('dollar5');
- let dollarCanadien = document.getElementById('dollarCanadien');
-let dollarcanadien5 = document.getElementById('dollarCanadien5');
-let livresterling = document.getElementById('livresterling');
-let livresterling5 = document.getElementById('livresterling5');
-let francuisse = document.getElementById('francuisse');
-let francuisse5 = document.getElementById('francsuisse5');
-
-///////////////////// SELECT ///////////
-let euroDollar = 1.13          // dollar //
- let euroCanadien = 1.43 // dollar canadien //
- let  euroLivre = 0.83 // livre sterling
- let euroFranc =  1.04 // franc suisse //
+const monnaie = document.getElementById('money1');
+const monnaie2 = document.getElementById('money2');
+const button = document.getElementById('button')
+const result = document.querySelectorAll('result');
 
 
- francsuisseEuro = 0.96    // euro //
-francsuisseDollar =  1.08  // Dollar //
-francsuisseCanadien =  1.37  // Canadien
-francsuisseLivre  =0.80  // Livre
-
-
-livresterlingFranc = 1.25  // Franc Suisse //
-livresterlingEuro  = 1.20  // Euro //
-livresterlingDollar  = 1.35 // Dollar //
-livresterlingCanadien = 1.71 // Canadien //
-
-
-
-dollaramericainEuro = 0.89 // euro //
-dollaramericainCanadien = 1.27 // Canadien //
-dollaramericainSterling = 0.74  // Sterling //
-dollaramericainFranc = 0.92 // Franc Suisse
-
-
-dollarcanadienSuisse = 0.73 // Suisse //
-dollarcanadienEuro = 0.70 // euro //
-dollardcanadienAmericain = 0.79 // Americain //
-dollardcanadienSterling = 0.58 // Sterling //
-
-///////////////// ECHANGE //////////
-class transaction {
-    constructor(valeur1,valeur2,taux) {
-        this.valeur1 = valeur1 ;
-        this.valeur2 = valeur2;
-        this.taux = taux ;
-   if(valeur1 < 2 ) {
-       addEventListener.valeur1();{
-           console.log("bonjour tout le monde")
-    }
-    
-    
-       
-        
-    
-   }
+const tauxDeChange = {
+    euro: 0.8,
+    dollard: 1,
+    dollardCanadien: 1.15,
+    livreSterling: 1.1,
+    francSuisse: 1.5
 }
 
-    }
+button.addEventListener('click',() => calculerlemontant()) 
+
+const calculerlemontant  = () => {
+    const selectmonnaie = tauxDeChange[money1.value];
+    const selectmonnaie1 = tauxDeChange [money2.value];
+    const montant = document.getElementById('montant').value;
 
 
-let prototype =  new transaction (euro,dollar5,euroDollar);
-/////:ALGORYTMNE  DEJA CONSTRUIT //////////
-nouvelleaction(euro,dollarCanadien5,euroCanadien);
-nouvelleaction(euro,livresterling5,euroLivre);
-nouvelleaction(euro,francuisse5,euroFranc);
-nouvelleaction(dollar,dollarcanadien5,dollaramericainCanadien);
-nouvelleaction(dollar,euro5,dollaramericainEuro);
-nouvelleaction(dollar,livresterling5,dollaramericainSterling);
-nouvelleaction(dollar,francsuisse5,dollaramericainFranc);
-nouvelleaction(francuisse,euro5,francsuisseEuro);
-nouveleaction(francuisse,dollarCanadien5,francsuisseCanadien);
-nouvelleaction(francuisse,dollar5,francsuisseDollar);
-nouvelleaction(francuisse,livresterling5,francsuisseLivre);
-nouvelleaction(livresterling,dollarCanadien,livresterlingCanadien,);
-nouvelleaction(livresterling,euro5,livresterlingDollar);
-nouvelleaction(livresterling,dollar5,livresterlingEuro);
-nouvelleaction(livresterling,francsuisse5,livresterlingFranc);
+const resultat =  montant * tauxDeChange[money1.value] * tauxDeChange[money2.value]
+document.getElementById("result").value = resultat ;
+
+
+
+console.log(resultat);
+console.log(selectmonnaie , selectmonnaie1) ;
+
+}
